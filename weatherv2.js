45bb9180d1a9a6f6d03f5c2e0b821bea05f81333
Fabio <fabioperestelo5@gmail.com>
2023-01-17 13:10:34 +0000

    update

--- weatherv2.js
+++ weatherv2.js
@@ -1,6 +1,6 @@
 const API_KEY = "a76aca2cbf214ceb941193109221012";
 
-async function getWeather(city) {
+async function getWeatherFromCityApi(city) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
   );
@@ -10,13 +10,12 @@ async function getWeather(city) {
 
 async function displayweather() {
   const city = document.getElementById("input").value;
-  const cityWeather = await getWeather(city);
+  const cityWeather = await getWeatherFromCityApi(city);
 
-  document.getElementById(
-    "currenttemp"
-  ).innerHTML = `${cityWeather.current.temp_c}°C`;
+  document.getElementById("currenttemp").innerHTML =
+    Math.round(cityWeather.current.temp_c) + "°C";
 
   document.getElementById(
     "currenticon"
-  ).innerHTML = `<img src="${cityWeather.current.condition.icon}">`;
+  ).innerHTML = `<img class="weathericon2" src="${cityWeather.current.condition.icon}">`;
 }
